ce701cad65e5a5146cb4a93587e4fe939d8c5548
André Luiz Vicente Silva <andre.silva@estudante.iftm.edu.br>
2025-07-15 00:25:32 -0300

    feat: add heroku profile

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # Carrega variáveis de ambiente do .env
-
+if os.getenv("ENV") != "production":
+    load_dotenv()
+    
 class Settings:
     PROJECT_NAME: str = "IoT Project Manager API"
     PROJECT_VERSION: str = "1.0.0"
 
     DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user:password@db_host:5432/iot_db")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey") # Use uma chave mais segura em produção
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey") 
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30 # Minutos para expiração do token
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30 
 
 settings = Settings()
